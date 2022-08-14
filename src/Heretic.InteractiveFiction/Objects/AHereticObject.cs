a1a57756b3e542a2a6cdf4223ca5f0e2d24dfb0d
Marc Biegota <marc.biegota@web.de>
2022-08-14 13:24:56 +0200

    refactor descriptions from AHereticObject

--- src/Heretic.InteractiveFiction/Objects/AHereticObject.cs
+++ src/Heretic.InteractiveFiction/Objects/AHereticObject.cs
@@ -57,66 +57,7 @@ public abstract partial class AHereticObject
     /// Used to define some grammar rules.
     /// </summary>
     public Grammars Grammar { get; set; }
-
-    /// <summary>
-    /// The detailed description of the object. It is used during printout.
-    /// </summary>
-    public Description Description { get; set; }
-
-    /// <summary>
-    /// The first look description is only used during the first printout and contains additional information.
-    /// </summary>
-    public Description FirstLookDescription { get; set; }
-    /// <summary>
-    /// The hint description is shown when the hint system has been activated.
-    /// </summary>
-    public Description Hint { get; set; }
-    /// <summary>
-    /// This description can be used to describe the discover situation in more detail. It is used during printout instead of the name of the object.
-    /// It is only valid in the context of the location where it was found and is deleted after a pickup.
-    /// </summary>
-    public Description ContainmentDescription { get; set; }
-    /// <summary>
-    /// Is shown when the item is broken.
-    /// </summary>
-    public Description BrokenDescription { get; set; }
-    /// <summary>
-    /// This description is used when the object cannot be broken.
-    /// If the description is empty, a default text is generated.
-    /// </summary>
-    public Description UnbreakableDescription { get; set; }
-    /// <summary>
-    /// If the object cannot be taken, this description can explain why.
-    /// </summary>
-    public Description UnPickAbleDescription { get; set; }
-    /// <summary>
-    /// If the object cannot be dropped, this description can explain why.
-    /// </summary>
-    public Description UnDropAbleDescription { get; set; }
-    /// <summary>
-    /// Gives a more detailed description about the state of a locked object.
-    /// </summary>
-    public Description LockDescription { get; set; }
-    /// <summary>
-    /// Gives a more detailed description about the state of an opened object.
-    /// </summary>
-    public Description OpenDescription { get; set; }
-    /// <summary>
-    /// Gives a more detailed description about the state of a closed object.
-    /// </summary>
-    public Description CloseDescription { get; set; }
-    /// <summary>
-    /// This description can be used if the object is linked to another.
-    /// </summary>
-    public Description LinkedToDescription { get; set; }
-    /// <summary>
-    /// This description can be used if the object is climbed by the player.
-    /// </summary>
-    public Description ClimbedDescription { get; set; }
-    /// <summary>
-    /// This description can be used if the object is readable.
-    /// </summary>
-    public Description LetterContentDescription { get; set; }
+    
     /// <summary>
     /// Can this object be broken?
     /// </summary>
@@ -156,14 +97,6 @@ public abstract partial class AHereticObject
     /// </summary>
     public bool IsClimbAble { get; set; }
     /// <summary>
-    /// The weight of the object.
-    /// </summary>
-    public int Weight { get; set; }
-    /// <summary>
-    /// If this object is a container, you can specify how much weight it can hold.
-    /// </summary>
-    public int MaxPayload { get; set; }
-    /// <summary>
     /// Determines whether simply looking at the surroundings will make this object visible.
     /// </summary>
     public bool IsUnveilAble { get; set; }
@@ -199,6 +132,16 @@ public abstract partial class AHereticObject
     /// If the object is a surrounding, then it will not be listed in the room description.
     /// </summary>
     public bool IsSurrounding { get; set; }
+    
+    /// <summary>
+    /// The weight of the object.
+    /// </summary>
+    public int Weight { get; set; }
+    /// <summary>
+    /// If this object is a container, you can specify how much weight it can hold.
+    /// </summary>
+    public int MaxPayload { get; set; }
+    
     /// <summary>
     /// The list of contained objects.
     /// </summary>
--- src/Heretic.InteractiveFiction/Objects/Item.cs
+++ src/Heretic.InteractiveFiction/Objects/Item.cs
@@ -2,7 +2,11 @@ namespace Heretic.InteractiveFiction.Objects;
 
 public sealed class Item : AHereticObject
 {
+    /// <summary>
+    /// Does this item prevent another item from being picked up?
+    /// </summary>
     public bool IsBlockingPickUp { get; set; }
+    
     public Item()
     {
         this.IsBlockingPickUp = false;
