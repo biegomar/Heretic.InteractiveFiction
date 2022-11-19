66309f89955ee1e2c4af59ca9f37307c8e75f4a5
Marc Biegota <marc.biegota@web.de>
2022-11-19 10:30:29 +0100

    make test project runable

--- test/Heretic.InteractiveFiction.TestGame/Program.cs
+++ test/Heretic.InteractiveFiction.TestGame/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Heretic.InteractiveFiction.GamePlay;
+using Heretic.InteractiveFiction.Grammars;
+using Heretic.InteractiveFiction.Objects;
+using Heretic.InteractiveFiction.Subsystems;
+using Heretic.InteractiveFiction.TestGame.GamePlay;
+using Heretic.InteractiveFiction.TestGame.Printing;
 
-Console.WriteLine("Hello, World!");
+IResourceProvider resourceProvider = new ResourceProvider();
+IPrintingSubsystem printingSubsystem = new ConsolePrinting();
+var universe = new Universe(printingSubsystem, resourceProvider);
+var eventProvider = new EventProvider(universe, printingSubsystem);
+IGamePrerequisitesAssembler gamePrerequisitesAssembler = new GamePrerequisitesAssembler(eventProvider);
+IGrammar grammar = new GermanGrammar(resourceProvider);
+var gameLoop = new GameLoop(printingSubsystem, universe, gamePrerequisitesAssembler, grammar);
+
+gameLoop.Run();
