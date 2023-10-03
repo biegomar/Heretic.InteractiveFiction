451d7d3738710bf0fb6997458bb682b63de1dda4
Marc Biegota <marc.biegota@web.de>
2023-10-03 12:13:17 +0200

    refactor AdventureEvent

--- src/Heretic.InteractiveFiction/Grammars/AdventureEvent.cs
+++ src/Heretic.InteractiveFiction/Grammars/AdventureEvent.cs
@@ -8,12 +8,6 @@ public sealed class AdventureEvent
     public AHereticObject? ObjectOne => AllObjects.FirstOrDefault();
     public AHereticObject? ObjectTwo => AllObjects.Skip(1).FirstOrDefault();
     
-    public List<AHereticObject> AllObjects { get; init; }
-    public List<string> UnidentifiedSentenceParts { get; init; }
-
-    public AdventureEvent()
-    {
-        this.UnidentifiedSentenceParts = new List<string>();
-        this.AllObjects = new List<AHereticObject>();
-    }
+    public List<AHereticObject> AllObjects { get; init; } = new();
+    public List<string> UnidentifiedSentenceParts { get; init; } = new();
 }
