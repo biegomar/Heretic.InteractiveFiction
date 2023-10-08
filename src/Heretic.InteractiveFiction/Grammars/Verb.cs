f819dbe495f807ce118673a012432f71a549530b
Marc Biegota <marc.biegota@web.de>
2023-10-08 15:41:08 +0200

    open verb error text to public setter

--- src/Heretic.InteractiveFiction/Grammars/Verb.cs
+++ src/Heretic.InteractiveFiction/Grammars/Verb.cs
@@ -7,5 +7,5 @@ public sealed class Verb
     public VerbKey Key { get; init; }
     public IEnumerable<VerbVariant> Variants { get; set; } = new List<VerbVariant>();
     public IEnumerable<PrepositionVariant> Prepositions { get; init; } = new List<PrepositionVariant>();
-    public Description ErrorMessage { get; init; } = string.Empty;
+    public Description ErrorMessage { get; set; } = string.Empty;
 }
